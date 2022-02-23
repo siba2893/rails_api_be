92fd7fd728b980adeca1518ac86277bb98d07c38
daniel.sibaja <siba2893@gmail.com>
2022-02-23 17:41:41 -0600

    Add Schema and changes to routes and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
+  extend Devise::Models #added this line to extend devise model
+
   devise :database_authenticatable,
          :registerable,
          :recoverable,
